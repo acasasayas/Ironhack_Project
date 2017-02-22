0ba0d2ece58410f509af11ba1564caf26b69744b
acasasayas <alexcasasayas@gmail.com>
2017-02-22 15:48:58 +0100

    Add First Folder

---
+++ project/app/controllers/clubs_controller.rb
@@ -0,0 +1,56 @@
+class ClubsController < ApplicationController
+  before_action :find_club
+
+  def index
+    clubs = Club.all
+    render json: clubs
+  end
+
+  def create
+    club = Club.create(club_params)
+    render json: club
+  end
+
+  def show
+    unless @club
+      club_not_found
+      return
+    end
+
+    render json: @club
+  end
+
+  def update
+    unless @club
+      club_not_found
+      return
+    end
+
+    @club.update(club_params)
+    render json: @club
+  end
+
+  def destroy
+    unless @club
+      club_not_found
+      return
+    end
+
+    @club.destroy
+    render json: @club
+  end
+
+  private
+
+  def club_params
+    params.permit(:name, :calories)
+  end
+
+  def find_club
+    @club = Club.find_by(id: params[:id])
+  end
+
+  def club_not_found
+    render json: {error: "club not found"}, status: 404
+  end
+end
