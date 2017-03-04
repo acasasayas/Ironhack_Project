f3d0608972510f3f9bab8e9ee79a20c0246e95f3
Carlos Castillo Ibañez <carloscastilloibanyez@gmail.com>
2017-03-04 11:41:12 +0100

    Create model ImageClub

---
+++ project/app/models/image_club.rb
@@ -0,0 +1,2 @@
+class ImageClub < ApplicationRecord
+end
---
+++ project/db/migrate/20170304104100_create_image_clubs.rb
@@ -0,0 +1,8 @@
+class CreateImageClubs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :image_clubs do |t|
+
+      t.timestamps
+    end
+  end
+end
