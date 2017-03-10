e8f59c8f28b2927baaf4af1ce8ca1cc80b1f34d3
Carlos Castillo Ibañez <carloscastilloibanyez@gmail.com>
2017-03-10 09:18:21 +0100

    modal login OK

---
+++ project/app/assets/javascripts/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(document).on("click", "#acceder", function () {
+    $("#modal-prueba").removeClass("hidden");
+    $("#modal-prueba").addClass("show");
+  });
+});
