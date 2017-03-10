791b3a5ce691cf6dc226342fe5b12f10f3f21f39
Carlos Castillo Ibañez <carloscastilloibanyez@gmail.com>
2017-03-10 15:45:24 +0100

    ok login,sign and new club modals

--- project/app/assets/javascripts/index.js
+++ project/app/assets/javascripts/index.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
 
-  $(document).on("click", "#acceder", function () {
-    event.preventDefault();
-    $("#modal-login").removeClass("hidden");
+  $(document).on("click", ".menu-open-button", function () {
+    $(".menu-item").removeClass("hidden");
   });
 
-  $(document).on("click", "#register", function () {
-    event.preventDefault();
-    $("#modal-register").removeClass("hidden");
-  });
-
-  $(document).on("click", "#map-canvas", function () {
-    event.preventDefault();
-    $("#modal-prueba").addClass("hidden");
-  });
 
 });
