49065ca82161a0d3ae81bb04734b980654d94f44
Carlos Castillo Ibañez <carloscastilloibanyez@gmail.com>
2017-03-11 11:52:57 +0100

    hide menu right when click in child

--- project/app/assets/javascripts/index.js
+++ project/app/assets/javascripts/index.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
+var $document = $(document);
+$document.ready(function(){
 
-  $(document).on("click", ".menu-open-button", function () {
+  $document.on("click", ".menu-open-button", function () {
     $(".menu-item").removeClass("hidden");
   });
 
-  $(document).on("click", ".menu-item", function () {
-    $("#menu-open").toggle();
+  $document.on("click", ".menu-item", function (e) {
+    $("#menu-open").prop('checked', false);
+    $(".menu-item").addClass("hidden");
   });
+
+
 
 });
